229f3bfd83320c0a17f6e64c48375655f865fe4d
Sam Shahriari <sampeson20@gmail.com>
2026-01-06 18:52:21 +0100

    cleanup

--- create_po3.py
+++ create_po3.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from typing import Iterable
 from dotenv import load_dotenv
 import pandas as pd
 
@@ -9,15 +10,28 @@ def write_file(file_name, lines):
         f.write("\n".join(lines))
 
 
-def generate_header_row() -> str:
-    return "MH00" + " " * (12 - 5 + 1) + os.getenv("ORG_NUMBER") + " " * (34 - 23 + 1) + os.getenv("ACCOUNT_NUMBER").ljust(10) + "SEK" + " " * (53 - 48 + 1) + "SEK" + " " * (80 - 57 + 1)
+def generate_start_line() -> str:
+    return (
+        "MH00"
+        + " " * (12 - 5 + 1)
+        + os.getenv("ORG_NUMBER")
+        + " " * (34 - 23 + 1)
+        + os.getenv("ACCOUNT_NUMBER").ljust(10)
+        + "SEK"
+        + " " * (53 - 48 + 1)
+        + "SEK"
+        + " " * (80 - 57 + 1)
+    )
 
 
-def generate_footer_row(number_of_rows: int, total_cost: float) -> str:
-    def format_kostnad(kostnad, length=15):
-        return str(int(kostnad * 100)).zfill(length)
-
-    return "MT00" + " " * (29 - 5 + 1) + str(number_of_rows).zfill(7) + format_kostnad(total_cost, 15) + " " * (80 - 52 + 1)
+def generate_end_line(number_of_rows: int, total_cost: float) -> str:
+    return (
+        "MT00"
+        + " " * (29 - 5 + 1)
+        + str(number_of_rows).zfill(7)
+        + format_kostnad(total_cost, 15)
+        + " " * (80 - 52 + 1)
+    )
 
 
 def format_kostnad(kostnad, length=13):
@@ -26,49 +40,70 @@ def format_kostnad(kostnad, length=13):
 
 def row_validation(row) -> bool:
     # TODO more checks
-
     return row["Godkänt"]
+
+
+def generate_PI00(clearing_number, account_number, amount, message) -> str:
+    return (
+        "PI00"
+        + "09"
+        + str(clearing_number).ljust(5)
+        + str(account_number).ljust(11)
+        + "  "
+        + datetime.datetime.now().strftime("%Y%m%d")
+        + format_kostnad(amount)
+        + message.ljust(20)
+        + " " * (80 - 66 + 1)
+    )
+
+
+def generate_BA00(note) -> str:
+    return (
+        "BA00"
+        + " " * (22 - 5 + 1)
+        + " " * (31 - 23 + 1)
+        + note.ljust(66 - 32 + 1)
+        + " " * (80 - 67 + 1)
+    )
+
+
+def generate_BE01(recipient) -> str:
+    return "BE01" + " " * (22 - 5 + 1) + recipient.ljust(80 - 23 + 1)
+
+
+def generate_lines_for_one_expense(row) -> Iterable[str] | None:
+    if not row_validation(row):
+        print(f"Row validation failed for row: {row}")
+        return None
+    out = []
+    message = f"{row['Verksamhet']} {row['Kort beskrivning av köp']}"
+    out.append(
+        generate_PI00(
+            row["Clearingnummer"], row["Kontonummer"], row["Kostnad"], message
+        )
+    )
+    note = f"{row['Verksamhet']} {row['Kort beskrivning av köp']} {row['Ditt namn']}"
+    out.append(generate_BA00(note))
+    out.append(generate_BE01(row["Ditt namn"]))
+    return out
 
 
 def main():
     load_dotenv()
 
-    number_of_rows = 0  # update if multiple rows
+    number_of_rows = 0
     total_cost = 0
 
-    content = [1]
-
     output_lines = []
-    output_lines.append(generate_header_row())
+    output_lines.append(generate_start_line())
     content = pd.read_csv(os.getenv("EXPENSE_PATH"))
     for _, row in content.iterrows():
-        if not row_validation(row):
-            print(f"Row validation failed for row: {row}")
-            continue
-        output_lines.append(
-            "PI00"
-            + "09"
-            + str(row["Clearingnummer"]).ljust(5)
-            + str(row["Kontonummer"]).ljust(11)
-            + "  "
-            + datetime.datetime.now().strftime("%Y%m%d")
-            + format_kostnad(row["Kostnad"])
-            + (f"{row['Verksamhet']} {row['Kort beskrivning av köp']}").ljust(20)
-            + " " * (80 - 66 + 1)
-        )  # meddelande kan också vara i BM99
-        output_lines.append(
-            "BA00"
-            + " " * (22 - 5 + 1)
-            + " " * (31 - 23 + 1)
-            + (f"{row['Verksamhet']} {row['Kort beskrivning av köp']} {row['Ditt namn']}").ljust(66 - 32 + 1)
-            + " " * (80 - 67 + 1)
-        )  # egen notering # bra om det är samma för samma bokföringspost och konto TODO längd
-        output_lines.append(
-            "BE01" + " " * (22 - 5 + 1) + str(row["Ditt namn"]).ljust(80 - 23 + 1)  # max 30
-        )  # denna läses inte in
-        total_cost += row["Kostnad"]
-        number_of_rows += 1
-    output_lines.append(generate_footer_row(number_of_rows, total_cost))
+        payment = generate_lines_for_one_expense(row)
+        if payment:
+            output_lines.extend(payment)
+            total_cost += row["Kostnad"]
+            number_of_rows += 1
+    output_lines.append(generate_end_line(number_of_rows, total_cost))
     file_name = "utlägg_" + datetime.datetime.now().strftime("%Y%m%d") + "_po3.txt"
     write_file(file_name, output_lines)
 

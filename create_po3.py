a7a9e508fdb6a5058ed8eaaaa9d0bf7bb0ae285e
Sam Shahriari <sampeson20@gmail.com>
2026-01-06 18:00:58 +0100

    use sheet columns names

--- create_po3.py
+++ create_po3.py
@@ -45,29 +45,28 @@ def main():
         if not row_validation(row):
             print(f"Row validation failed for row: {row}")
             continue
-        row_validation(row)
         output_lines.append(
             "PI00"
             + "09"
-            + str(row["clearing"]).ljust(5)
-            + str(row["account_number"]).ljust(11)
+            + str(row["Clearingnummer"]).ljust(5)
+            + str(row["Kontonummer"]).ljust(11)
             + "  "
             + datetime.datetime.now().strftime("%Y%m%d")
-            + format_kostnad(row["amount"])
-            + (row["project"]+row["account"]).ljust(20)
+            + format_kostnad(row["Kostnad"])
+            + (f"{row['Verksamhet']} {row['Kort beskrivning av köp']}").ljust(20)
             + " " * (80 - 66 + 1)
         )  # meddelande kan också vara i BM99
         output_lines.append(
             "BA00"
             + " " * (22 - 5 + 1)
             + " " * (31 - 23 + 1)
-            + (row["project"]+row["account"]+row["name"]).ljust(66 - 32 + 1)
+            + (f"{row['Verksamhet']} {row['Kort beskrivning av köp']} {row['Ditt namn']}").ljust(66 - 32 + 1)
             + " " * (80 - 67 + 1)
-        )  # egen notering # bra om det är samma för samma bokföringspost och konto
+        )  # egen notering # bra om det är samma för samma bokföringspost och konto TODO längd
         output_lines.append(
-            "BE01" + " " * (22 - 5 + 1) + str(row["amount"]).ljust(80 - 23 + 1)  # max 30
+            "BE01" + " " * (22 - 5 + 1) + str(row["Ditt namn"]).ljust(80 - 23 + 1)  # max 30
         )  # denna läses inte in
-        total_cost += row["amount"]
+        total_cost += row["Kostnad"]
         number_of_rows += 1
     output_lines.append(generate_footer_row(number_of_rows, total_cost))
     file_name = "utlägg_" + datetime.datetime.now().strftime("%Y%m%d") + "_po3.txt"
